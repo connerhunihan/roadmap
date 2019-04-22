9b97197e727f640860d7b8b270116edb45035092
Mahmoud Hamsho <36112117+hamshom@users.noreply.github.com>
2019-04-22 00:17:43 -0700

    Add files via upload

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, abort
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
@@ -21,27 +21,36 @@ db = pymongo.database.Database(client, 'roadmap')
 col = pymongo.collection.Collection(db, 'test')
 
 
-@app.route('/star', methods=['GET'])
-def get_all_stars():
+@app.route('/tasks/<task_id>', methods=['GET'])
+def get_task(task_id):
 
-    output = []
-    for s in col.find().limit(5):
-        output.append({'ID' : s['_id'], 'Task': s['task']})
+    if task_id == 'None':
+
+        output = []
+        for s in col.find():
+            output.append({'ID' : s['_id'], 'Task': s['task']})
+    else:
+        task_id = int(task_id)
+        output = [task for task in col.find() if task['_id'] == task_id]
+
 
     return jsonify({'result' : output})
 
 
-@app.route('/add', methods=['POST'])
-def add_star():
+
+## Still working on this part###########3333
+@app.route('/tasks', methods=['POST'])
+def add_task():
+
+    lastRecord = col.find().limit(1).sort({'_id': 1})
+
     if request.headers['Content-Type'] == 'application/json':
 
         arguments = request.get_json()
-
-        id = arguments.get("id")
         task = arguments.get("task")
 
         data = {
-            "_id": int(id) ,
+            "_id": int(lastRecord) + 1,
             "task": str(task)
         }
 
@@ -49,6 +58,43 @@ def add_star():
         resp = Response(json.dumps(data), mimetype='application/json')
 
     return resp
+
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in col.find() if task['_id'] == task_id]
+
+    db_response = col.delete_one(task[0])
+
+    if db_response.deleted_count == 1:
+        response = {'ok': True, 'message': 'record deleted'}
+    else:
+        response = {'ok': True, 'message': 'no record found'}
+
+    return jsonify({'result': response})
+
+
+## Still working on this part###########3333
+@app.route('/v1/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in col.find() if task['_id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['is_completed'] = request.json.get('is_completed', task[0]['is_completed'])
+    return jsonify({'task': task[0]})
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 
 
 if __name__ == "__main__":
